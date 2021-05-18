0cd4669f6177da0f4a79d7287c8ea72bb8367da2
jonathon McClendon <jonathonsmcclendon@gmail.com>
2021-05-18 16:24:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 
 function writeCards(array){
-  for (i = 0; i < array.length; i ++){
+  for (let i = 0; i < array.length; i ++){
     console.log(`Wrapped ${array[i]} and added a bow!`);
   }
 }
